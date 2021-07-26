18dd3b4a28447e68b6170e0639de57b14f872103
Gleibson Alves <alvesgleibson@gmail.com>
2021-07-26 15:52:10 -0300

    Department entity class

---
+++ src/application/program.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.entities.Department;
+
+public class program {
+	
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Tecnology");
+		
+		System.out.println(obj);
+		
+		
+		
+	}
+
+}
