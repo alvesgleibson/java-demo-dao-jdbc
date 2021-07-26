7a5318380ba7070cf299e4f88431491358e09ed1
Gleibson Alves <alvesgleibson@gmail.com>
2021-07-26 16:52:17 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/program.java
+++ src/application/program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,9 @@ public class Program {
 		Department obj = new Department(1, "Tecnology");
 		
 		Seller seller = new Seller(22, "alvesgleibson@gmail.com","alvesgleibson@gmail.com", new Date(),5500.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		
 		System.out.println(obj+"\n");
 		System.out.println(seller);
