b0db12d7e40baa82f4872310175cf05167135896
Gleibson Alves <alvesgleibson@gmail.com>
2021-07-27 22:11:37 -0300

    findByDepartment implementation

--- src/application/program.java
+++ src/application/program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,15 +13,24 @@ public class Program {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
-		System.out.println("########## Test 1: Seller FindById ###########\n");
+		System.out.println("================ Test 1: Seller FindById ================\n");
 		Seller sellerSoftwareEngineer = sellerDao.findById(3);
 
 		if (sellerSoftwareEngineer != null) {
 			System.out.println(sellerSoftwareEngineer);
 		} else
 			System.out.println("Id procurado não encontrado no Seller");
-		
-		//Fim Find seller by id
+
+		// End find seller by id
+
+		System.out.println("\n\n================ Test 2: Seller FindByDepartment ================\n");
+		List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+
+		for (Seller obj : list) {
+
+			System.out.println(obj);
+
+		}
 
 	}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -55,7 +58,6 @@ public class SellerDaoJDBC implements SellerDao {
 
 			if (rs.next()) {
 
-				
 				Department dp = instatiateDepartment(rs);
 				Seller seller = instatiateSeller(rs, dp);
 
@@ -85,7 +87,7 @@ public class SellerDaoJDBC implements SellerDao {
 		seller.setBirthDate(rs.getDate("BirthDate"));
 		seller.setBaseSalary(rs.getDouble("BaseSalary"));
 		seller.setDepartment(dp);
-		
+
 		return seller;
 	}
 
@@ -96,10 +98,52 @@ public class SellerDaoJDBC implements SellerDao {
 		dp.setName(rs.getString("DepName"));
 		return dp;
 	}
-	
-	
-	
-	
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name");
+
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+
+			List<Seller> sellerList = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+
+			while (rs.next()) {
+
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+				if (dep == null) {
+
+					dep = instatiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+
+				Seller seller = instatiateSeller(rs, dep);
+
+				sellerList.add(seller);
+
+			}
+			return sellerList;
+
+		} catch (SQLException e) {
+
+			throw new DbException("Erro SQL, Cause by: " + e.getMessage());
+		}
+
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+
+		}
+	}
 
 	@Override
 	public List<Seller> findAll() {
